e13a0c0a38af9a1ffaa5de65d7b074599a502e91
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2023-07-28 02:30:49 +0530

    Practical 02

---
+++ Practical 02/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*01
+    int age;
+    printf("HI, HOW OLD ARE YOU? ”);
+    scanf("%d”, &age);
+    printf("\n \n ”);
+
+    printf("WELCOME %d \n ”, age);
+    printf("LET’S BE FRIENDS! \n ”);*/
+
+    /*02
+    printf("%15d %15d %15d \n ”, 2,4,6);
+    printf("%15d %15d %15d \n ”, 3,9,27);
+    printf("%15d %15d %15d \n ”, 4,16,64);
+
+    return 0;*/
+
+    /*03
+    int d,t;
+    float avgs;
+
+	printf("Enter the distance travelled in meters : ”);
+    scanf("%d", &d);
+	printf("Enter the time taken in seconds : ”);
+	scanf("%d”, &t);
+
+	avgs=d/t;
+
+	printf("The average speed is m/s %.3f \n ”, avgs);
+
+	return0;*/
+
+      /*04
+      float tempf,tempc,t;
+
+      printf("Enter the temperature in Fahrenheit : ”);
+      scanf("%f”, &tempf);
+
+      t=(tempf-32);
+      tempc=(t*0.55);
+
+      printf("Temperature in Celsius : %.3f \n ”tempc);
+
+      return 0;*/
+
+
+
+
+
+
+}
