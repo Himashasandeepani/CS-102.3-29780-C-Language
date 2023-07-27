0f560d482b2d8f46c6f3966df4bd9802db131584
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2023-07-28 01:30:10 +0530

    Practical 01

---
+++ Practical 1/main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /*01
+  printf("Y.G.Himasha Sandeepani \n );
+  printf("H/Tangalle Balika Vidyalaya \n ”);*/
+
+  /*02
+  printf("* \n ”);
+  printf("** \n ”);
+  printf("*** \n ”);
+  printf("**** \n ”);
+  printf("***** \n ”);*/
+
+    /*03
+    int i;
+    float f;
+    double d;
+    char c;
+
+    printf("Enter a character : ”);
+    scanf("%c”, &c);
+    printf("Enter an integer : ”);
+    scanf("%d”, &i);
+    printf("Enter a float : ”);
+    scanf("%f”, &f);
+    printf("Enter a double : ”);
+    scanf("%lf”, &d);
+
+    printf("Integer : %d \n ”,i);
+    printf("Float : %f \n ”,f);
+    printf("Double : %f \n ”,d);
+    printf("Character : %c \n ”,c);*/
+
+   /*04
+   int n1,n2,tot;
+
+   printf("Enter the first number : ”);
+   scanf("%d”, &n1);
+   printf("Enter the second number : ”);
+   scanf("%d”, &n2);
+
+   tot=n1+n2;
+
+   printf("Total is %d ”,tot);*/
+
+   /*05
+   float n1,n2,avg;
+   printf("Enter the first number : \n ”);
+   scanf("%f”, &n1);
+   printf("Enter the second number : \n ”);
+   scanf("%f”,&n2);
+
+   avg=(n1+n2) /2;
+
+   printf("The average is %.2f \n ”,avg);*/
+
+   /*06
+   char name[20];
+   int byear,age;
+   printf("Enter student name : ”);
+   scanf("%s”, &name);
+   printf("Enter birth year : ”);
+   scanf("%d”,&byear);
+
+   age=2023-byear;
+
+   printf("Student name is %s \n ”, name);
+   printf("Age : %d \n ”, age);*/
+
+    /*07
+    int n1,n2;
+    printf("Enter first number : ”);
+    scanf("%d”, &n1);
+    printf("Enter second number : ”);
+    scanf("%d”, &n2);
+           //before swap
+   printf("The first number is %d \n ”, n1);
+   printf("The second number is %d \n ”, n2);
+          //after swap
+   printf("The second number is %d \n ”, n2);
+   printf("The first number is %d \n ”, n1);*/
+
+
+}
