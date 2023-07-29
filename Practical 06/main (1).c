5968c02226b34d75c2423f03d442db250fe3956f
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2023-07-29 18:14:10 +0530

    Practical 06

---
+++ Practical 06/main (1).c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   /* int arr[10],max,min,tot,i;
+    float avg;
+
+    for(i=0;i<10;i++)
+    {
+      printf("Enter %d number -",i+1);
+      scanf("%d",&arr[i]);
+
+      tot=tot+arr[i];
+    }
+    max=arr[0];
+    min=arr[0];
+
+    for(i=1;i<10;i++)
+    {
+        if(arr[i]>max)
+        max=arr[i];
+      if(arr[i]<min)
+        min=arr[i];
+    }
+     avg=(float)tot/10.0;
+
+
+    for(i=0;i<10;i++)
+        printf("Entered %d element is %d \n",i+1,arr[i]);
+    printf("The maximam is %d \n",max);
+    printf("The minimam is %d \n",min);
+    printf("The average is %.2f \n",avg);
+
+    for(i=9;i>=0;i--)
+        printf("%d \t",arr[i]);*/
+}
